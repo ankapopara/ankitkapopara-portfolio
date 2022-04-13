e797b20a747a948897a8d26cc2cae63913858e29
kapopara <kapopara.ankit@gmail.com>
2022-04-13 12:36:26 +0530

    new version added 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Designer", "Freelancer"],
+        strings: ["Front-End Web Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -9,41 +9,6 @@ $(document).ready(function(){
 
     // INITIALIZE AOS
     AOS.init();
-
-    //Number count
-    (function($) {
-        $.fn.countTo = function(options) {
-        return this.each(function() {
-            //-- Arrange
-            var FRAME_RATE = 60; // Predefine default frame rate to be 60fps
-            var $el = $(this);
-            var countFrom = parseInt($el.attr('data-count-from'));
-            var countTo = parseInt($el.attr('data-count-to'));
-            var countSpeed = $el.attr('data-count-speed'); // Number increment per second
-    
-            //-- Action
-            var rafId;
-            var increment;
-            var currentCount = countFrom;
-            var countAction = function() {              // Self looping local function via requestAnimationFrame
-            if(currentCount < countTo) {              // Perform number incremeant
-                $el.text(Math.floor(currentCount));     // Update HTML display
-                increment = countSpeed / FRAME_RATE;    // Calculate increment step
-                currentCount += increment;              // Increment counter
-                rafId = requestAnimationFrame(countAction);
-            } else {                                  // Terminate animation once it reaches the target count number
-                $el.text(countTo);                      // Set to the final value before everything stops
-                //cancelAnimationFrame(rafId);
-            }
-            };
-            rafId = requestAnimationFrame(countAction); // Initiates the looping function
-        });
-        };
-    }(jQuery));
-    
-    //-- Executing
-    $('.counter').countTo();
-
 
     $(window).scroll(function(){
         // scroll-up button show/hide script
@@ -59,24 +24,34 @@ $(document).ready(function(){
         $('html').animate({scrollTop: 0});
     });
 
+    //Counter
+    var counted = 0;
+    $(window).scroll(function() {
+        var oTop = $('#counters').offset().top - window.innerHeight;
+        if (counted == 0 && $(window).scrollTop() > oTop) {
+            $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+                }, 
+                {
+                duration: 2000,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+                });
+            });
+            counted = 1;
+        }
+    });
+
+
+
 });
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     const observer = new IntersectionObserver(entries => {
-//         entries.forEach(entry => {
-//             const id = entry.target.getAttribute('id');
-//             if (entry.intersectionRatio > 0) {
-//                 document.querySelector(`nav li a[href="#${id}"]`)
-//                 .parentElement.classList.add('active');
-//             } else {
-//                 document.querySelector(`nav li a[href="#${id}"]`)
-//                 .parentElement.classList.remove('active');
-//             }
-//         });
-//     });
-//     // Track all sections that have an `id` applied
-//     document.querySelectorAll('section[id]').forEach((section) => {
-//         observer.observe(section);
-//     });
-// });
